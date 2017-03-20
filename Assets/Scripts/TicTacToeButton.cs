dc69a5b635cf728d51a0031f8d56f1586cab7460
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2017-03-20 11:04:04 -0400

    Set up button logic

---
+++ Assets/Scripts/TicTacToeButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TicTacToeButton : MonoBehaviour {
+
+    public PlayerManager player;
+    public Text text;
+
+    private Button button;
+    private bool isTaken;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetTile);
+    }
+
+	private void SetTile()
+    {
+        if (!isTaken)
+        {
+            if (player.isX)
+            {
+                text.text = "X";
+            }
+            else
+            {
+                text.text = "O";
+            }
+
+            isTaken = true;
+        }
+    }
+}
